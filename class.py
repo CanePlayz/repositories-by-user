7c3e32dcb41a984d264338a72f825df9968a34ff
CanePlayz <jjpfst@gmail.com>
2023-02-09 02:03:02 +0100

    Fixed typo, groundwork for user/org support

--- class.py
+++ class.py
@@ -6,23 +6,27 @@ import utilities.selenium as selenium
 
 
 class Organization(object):
-    def __init__(self, name):
+    def __init__(self, name, org):
         self.name = name
+        self.org = org
 
     def excel_file_api(self):
         start = time.time()
         res = api.get_repo_info(self.name)
         print("Runtime: ", time.time() - start)
 
-    def excel_file_selentium(self):
+    def excel_file_selenium(self):
         start = time.time()
         res = selenium.get_repo_info(self.name)
         ex.write_to_excel(self.name, res)
         print("Runtime: ", time.time() - start)
 
 
-microsoft = Organization("microsoft")
+# Examples
 
-powershell = Organization("PowerShell")
+microsoft = Organization(name="Microsoft", org=True)
+powershell = Organization(name="PowerShell", org=True)
+caneplayz = Organization(name="CanePlayz", org=False)
 
-powershell.excel_file_selentium()
+
+caneplayz.excel_file_selenium()
